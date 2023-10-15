44abe9ccb3e6e8f7a2dd119b771c7151b4877eed
Tien Han <tienthuyhan@gmail.com>
2023-10-15 14:59:49 -0700

    Build out interactable and boundaries

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 50.0
-
+var interactable = []
 
 func _physics_process(delta):
-
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var lr_direction = Input.get_axis("ui_left", "ui_right")
@@ -23,3 +20,19 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
+
+func _on_interactor_area_entered(area):	
+	interactable.append(area.get_parent())
+
+func _on_interactor_area_exited(area):
+	interactable.clear()
+	
+func _input(event):
+	#TO-DO cover case where player has no interactables but presses e
+	if event.is_action_pressed("interact_key"):
+		print("Interact key was pressed")
+		interact_with_object(interactable[0])
+		
+func interact_with_object(interactable_object):
+	print(interactable_object.name)
+		
---
+++ Props/Interactable.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+@export var dialogue: String = "Heyyyy I got touched"
