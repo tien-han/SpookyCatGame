41fc5d470ca618d71ea1f3d304249deb0fb30f18
Tien Han <tienthuyhan@gmail.com>
2023-10-15 20:16:58 -0700

    Add timer to dialogue box

---
+++ Game/DialogueBox.gd
@@ -0,0 +1,9 @@
+extends PanelContainer
+
+func print_dialogue(dialogue):
+	$Dialogue.text = dialogue
+	self.visible = true
+	$Timer.start()
+
+func _on_timer_timeout():
+	self.visible = false
--- Game/scene.gd
+++ Game/scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_player_interacted_with(interactable):
-	$Canvas/DialogueBox/Dialogue.text = interactable.dialogue
+	$Canvas/DialogueBox.print_dialogue(interactable.dialogue)
